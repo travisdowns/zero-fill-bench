2bd72fc17f13c763377e4776feb24fcbea410004
Travis Downs <travis.downs@gmail.com>
2020-05-11 19:11:08 -0400

    collect alt01 in data.sh

--- scripts/data.sh
+++ scripts/data.sh
@@ -9,6 +9,6 @@ d='---------------------------'
 # up to CPU-count threads
 echo -e "$d\nCollecting data in $RDIR\n$d"
 ./bench --algos=fill0,fill1 --csv > "$RDIR/overall.csv"
-./bench --algos=fill0,fill1 --min-size=40000  --max-size=100000000 --perf-cols=l2-out-silent,l2-out-non-silent --step=1.1 --csv > "$RDIR/l2-focus.csv"
-./bench --algos=fill0,fill1 --min-size=300000 --max-size=400000000 --perf-cols=uncR,uncW --step=1.1 --csv > "$RDIR/l3-focus.csv"
+./bench --algos=fill0,fill1,alt01 --min-size=40000  --max-size=100000000 --perf-cols=l2-out-silent,l2-out-non-silent --step=1.1 --csv > "$RDIR/l2-focus.csv"
+./bench --algos=fill0,fill1,alt01 --min-size=300000 --max-size=400000000 --perf-cols=uncR,uncW --step=1.1 --csv > "$RDIR/l3-focus.csv"
 echo -e "$d\nData collection finished!\n$d"
